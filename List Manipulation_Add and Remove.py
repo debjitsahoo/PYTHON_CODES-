7e04ad48d48ca2a2cf6b738044e32b64b0c48de5
debjitsahoo <sahoodebjit660@gmail.com>
2026-02-10 22:50:18 +0530

    Add files via upload

---
+++ List Manipulation_Add and Remove.py
@@ -0,0 +1,9 @@
+fruits = ["apple","banana","cherry","date","elderberry"]
+print(f"Original List :{fruits}")
+
+fruits.append("fig")     # Add new fruit in end of list 
+
+fruits.pop(1)             # Delete second fruit 
+
+print(f"New List:{fruits}")
+
