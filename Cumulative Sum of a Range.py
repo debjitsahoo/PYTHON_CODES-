3a26b02885fb2474295de6972c6fd04ed5f83b98
debjitsahoo <sahoodebjit660@gmail.com>
2026-02-10 13:33:41 +0530

    Add files via upload

---
+++ Cumulative Sum of a Range.py
@@ -0,0 +1,7 @@
+n = int(input("Enter range for Cumulative Sum:"))
+prev_num = 0
+for i in range(n+1):
+    sum = prev_num + i
+    print(f"Current number {i} Previous number {prev_num} Sum:{sum}")
+
+    prev_num = i           # Update previous number value
